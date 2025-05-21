fbabd199595df008be885586325b85ff7404a61f
stasiaaleks <anastasiia@newtonideas.com>
2025-05-21 17:24:52 +0300

    add swagger generation

--- ShopApp.App/Program.cs
+++ ShopApp.App/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 string connectionString = builder.Configuration.GetConnectionString("DevDBConnection") 
                           ?? throw new InvalidOperationException("Connection string 'DevDBConnection' not found.");
 
-
 // minimal current cookie-based auth model
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options => { options.LoginPath = "/login"; });
@@ -18,14 +17,26 @@ builder.Services.AddSingleton(connectionString);
 builder.Services.AddDataServices();
 builder.Services.AddBusinessLogicServices();
 builder.Services.AddQueryProviders();
+builder.Services.AddEntityRepositories();
 
+// 3rd-party dependencies setup
 Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.UseRouting();
 app.MapControllers();
-app.Run();
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
