210748029e418593ce9cb929389c0ce28d5f716c
stasiaaleks <anastasiia@newtonideas.com>
2025-05-23 09:26:44 +0300

    remove sequence reset

--- ShopApp.App/Data/SQL/Employee/delete_by_id.sql
+++ ShopApp.App/Data/SQL/Employee/delete_by_id.sql
@@ -1,2 +1,1 @@
 DELETE FROM employee WHERE id_employee=@Id;
-SELECT SETVAL('public.employee_id_employee_seq',(SELECT COUNT(*) FROM employee)); -- to reset the ids sequence
--- ShopApp.App/Data/SQL/User/delete_by_id.sql
+++ ShopApp.App/Data/SQL/User/delete_by_id.sql
@@ -1,2 +1,1 @@
 DELETE FROM "user" WHERE user_id=@Id;
-SELECT SETVAL('public.user_user_id_seq',(SELECT COUNT(*) FROM "user")); -- to reset the ids sequence
