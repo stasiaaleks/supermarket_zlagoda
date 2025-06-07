202c14d23ebc8f69d207c5987b04a8edabd90e3d
stasiaaleks <anastasiia@newtonideas.com>
2025-06-07 13:51:59 +0300

    fix unapplied migration file

--- ShopApp.CLI/Migrations/migrations_sql/19_add_positive_integers_constraints.sql
+++ ShopApp.CLI/Migrations/migrations_sql/19_add_positive_integers_constraints.sql
@@ -5,12 +5,12 @@ ALTER TABLE customer_card
 ADD CONSTRAINT percent_check CHECK (percent > 0);
 
 ALTER TABLE sale
-ADD CONSTRAINT number_check CHECK (product_number > 0),
-ADD CONSTRAINT price_check CHECK (selling_price > 0);
+ADD CONSTRAINT number_check CHECK (product_number >= 0),
+ADD CONSTRAINT price_check CHECK (selling_price >= 0);
 
 ALTER TABLE store_product
-ADD CONSTRAINT number_check CHECK (products_number > 0),
-ADD CONSTRAINT price_check CHECK (selling_price > 0);
+ADD CONSTRAINT number_check CHECK (products_number >= 0),
+ADD CONSTRAINT price_check CHECK (selling_price >= 0);
 
 ALTER TABLE "check"
 ADD CONSTRAINT sum_check CHECK (sum_total > 0),
