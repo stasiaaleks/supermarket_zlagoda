3264cb67b7ebaf747bb7897228c0e5a179177dc2
stasiaaleks <anastasiia@newtonideas.com>
2025-06-07 13:23:18 +0300

    fix non-promotional products sql

--- ShopApp.App/Data/SQL/StoreProduct/get_all_regular.sql
+++ ShopApp.App/Data/SQL/StoreProduct/get_all_regular.sql
@@ -1,4 +1,8 @@
 ﻿SELECT st.*, product_name, characteristics
 FROM store_product st
 INNER JOIN public.product p ON p.id_product = st.id_product
-WHERE upc_prom IS NULL
+WHERE st.upc_prom IS NULL AND st.upc NOT IN (
+    SELECT st_prom.upc_prom
+    FROM store_product st_prom
+    WHERE st_prom.upc_prom IS NOT NULL
+)
