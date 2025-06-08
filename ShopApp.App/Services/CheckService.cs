d993b381e5a5594a72734b76c2f6362e50765322
stasiaaleks <anastasiia@newtonideas.com>
2025-06-08 21:27:06 +0300

    allow datetime values to be nullable

--- ShopApp.App/Services/CheckService.cs
+++ ShopApp.App/Services/CheckService.cs
@@ -1,4 +1,3 @@
-using System.Transactions;
 using AutoMapper;
 using ShopApp.DAL.Repository;
 using ShopApp.Data.DTO;
@@ -10,10 +9,10 @@ namespace ShopApp.Services;
 public interface ICheckService
 {
     Task<IEnumerable<CheckDto>> GetAll();
-    Task<IEnumerable<CheckWithSalesListDto>> GetAllWithSalesByPeriod(DateTime start, DateTime end);
-    Task<IEnumerable<CheckWithSalesListDto>> GetAllWithSalesByPeriodAndCashier(DateTime start, DateTime end, string cashierId);
-    Task<CheckSumDto> GetSumByPeriod(DateTime start, DateTime end);
-    Task<CheckSumDto> GetSumByEmployeeAndPeriod(DateTime start, DateTime end, string cashierId);
+    Task<IEnumerable<CheckWithSalesListDto>> GetAllWithSalesByPeriod(DateTime? start, DateTime? end);
+    Task<IEnumerable<CheckWithSalesListDto>> GetAllWithSalesByPeriodAndCashier(DateTime? start, DateTime? end, string cashierId);
+    Task<CheckSumDto> GetSumByPeriod(DateTime? start, DateTime? end);
+    Task<CheckSumDto> GetSumByEmployeeAndPeriod(DateTime? start, DateTime? end, string cashierId);
     Task<CheckWithSalesListDto> GetByNumberWithSales(string number);
 }
 
@@ -29,46 +28,57 @@ public class CheckService : ICheckService
         _mapper = mapper;
         _checkRepo = checkRepo;
     }
-    
+
     public async Task<IEnumerable<CheckDto>> GetAll()
     {
         return await _checkRepo.GetAllAsync<CheckDto>(_queryProvider.GetAll);
     }
 
-    public async Task<IEnumerable<CheckWithSalesListDto>> GetAllWithSalesByPeriod(DateTime start, DateTime end)
+    public async Task<IEnumerable<CheckWithSalesListDto>> GetAllWithSalesByPeriod(DateTime? start, DateTime? end)
     {
+        var (validStart, validEnd) = ValidateNullDatetimeValues(start, end);
         var query = _queryProvider.GetAllWithSalesByPeriod;
-        var checks = await _checkRepo.GetAllAsync<CheckWithSaleDto>(query, new { StartDate = start, EndDate = end });
+        var checks = await _checkRepo.GetAllAsync<CheckWithSaleDto>(query, new { StartDate = validStart, EndDate = validEnd });
         return _mapper.Map<IEnumerable<CheckWithSalesListDto>>(checks);
     }
 
-    public async Task<IEnumerable<CheckWithSalesListDto>> GetAllWithSalesByPeriodAndCashier(DateTime start, DateTime end, string cashierId)
+    public async Task<IEnumerable<CheckWithSalesListDto>> GetAllWithSalesByPeriodAndCashier(DateTime? start, DateTime? end, string cashierId)
     {
+        var (validStart, validEnd) = ValidateNullDatetimeValues(start, end);
         var query = _queryProvider.GetAllWithSalesByPeriodAndEmployee;
-        var checks = await _checkRepo.GetAllAsync<CheckWithSaleDto>(
-            query, new { StartDate = start, EndDate = end, EmployeeId = cashierId });
+        var parameters = new { StartDate = validStart, EndDate = validEnd, EmployeeId = cashierId };
+        var checks = await _checkRepo.GetAllAsync<CheckWithSaleDto>(query, parameters);
         return _mapper.Map<IEnumerable<CheckWithSalesListDto>>(checks);
     }
 
-    public async Task<CheckSumDto> GetSumByPeriod(DateTime start, DateTime end)
+    public async Task<CheckSumDto> GetSumByPeriod(DateTime? start, DateTime? end)
     {
+        var (validStart, validEnd) = ValidateNullDatetimeValues(start, end);
         var query = _queryProvider.GetSumByPeriod;
-        var parameters = new { StartDate = start, EndDate = end };
+        var parameters = new { StartDate = validStart, EndDate = validEnd };
         return await _checkRepo.GetSingleAsync<CheckSumDto>(query, parameters);
     }
-    
+
+    public async Task<CheckSumDto> GetSumByEmployeeAndPeriod(DateTime? start, DateTime? end, string cashierId)
+    {
+        var (validStart, validEnd) = ValidateNullDatetimeValues(start, end);
+        var query = _queryProvider.GetSumByPeriodAndEmployee;
+        var parameters = new { StartDate = validStart, EndDate = validEnd, EmployeeId = cashierId };
+        return await _checkRepo.GetSingleAsync<CheckSumDto>(query, parameters);
+    }
+
     public async Task<CheckWithSalesListDto> GetByNumberWithSales(string number)
     {
         var query = _queryProvider.GetByNum;
         var parameters = new { CheckNumber = number };
         var checks = await _checkRepo.GetAllAsync<CheckWithSaleDto>(query, parameters);
-        return _mapper.Map<IEnumerable<CheckWithSalesListDto>>(checks).ToList()[0];
+        return _mapper.Map<IEnumerable<CheckWithSalesListDto>>(checks).First();
     }
 
-    public async Task<CheckSumDto> GetSumByEmployeeAndPeriod(DateTime start, DateTime end, string cashierId)
+    private (DateTime start, DateTime end) ValidateNullDatetimeValues(DateTime? start, DateTime? end)
     {
-        var query = _queryProvider.GetSumByPeriodAndEmployee;
-        var parameters = new { StartDate = start, EndDate = end, EmployeeId = cashierId };
-        return await _checkRepo.GetSingleAsync<CheckSumDto>(query, parameters);
+        var validStart = start ?? DateTime.MinValue;
+        var validEnd = end ?? DateTime.UtcNow.Date.AddDays(1);
+        return (validStart, validEnd);
     }
 }
