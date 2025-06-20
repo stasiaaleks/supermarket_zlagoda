ddbc6c808374aa2e3bbe7701f65986eea1e253e4
stasiaaleks <anastasiia@newtonideas.com>
2025-06-20 22:02:49 +0300

    change route parameter to query parameter

--- ShopApp.App/Controllers/StatisticsController.cs
+++ ShopApp.App/Controllers/StatisticsController.cs
@@ -22,9 +22,9 @@ public class StatisticsController: ControllerBase
         return Ok(data);
     }
     
-    [HttpGet("same-checks-as/{surname}")]
+    [HttpGet("same-checks-as/")]
     [ProducesResponseType(typeof(IEnumerable<CashierCheckData>), StatusCodes.Status200OK)]
-    public async Task<IActionResult> GetCashiersWithSameChecks([FromRoute] string surname)
+    public async Task<IActionResult> GetCashiersWithSameChecks([FromQuery] string surname)
     {
         var data = await _statisticsService.GetCashiersWithSameChecks(surname);
         return Ok(data);
