56153aab72b3265e7e9db25128c493f77184fa5c
stasiaaleks <anastasiia@newtonideas.com>
2025-06-20 21:22:11 +0300

    fix typo

--- ShopApp.App/Data/SQL/Statistics/same_checks_as_cashier_by_surname.sql
+++ ShopApp.App/Data/SQL/Statistics/same_checks_as_cashier_by_surname.sql
@@ -22,7 +22,7 @@ WHERE e.surname != @Surname
     FROM check AS cp
              INNER JOIN sale AS sp ON cp.check_number = sp.check_number
     WHERE cp.id_employee IN (
-        SELECT id_employeen
+        SELECT id_employee
         FROM employee
         WHERE surname = @Surname
     )
